7efdaf286d04f87020b81bd6f34a947b592f5fcf
PrashantR30 <prashant@adg.io>
2025-10-29 18:30:23 +0400

    update main go - unsuc test

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
+// Successful test
+// package main
+
+// // Add returns a + b
+// func Add(a, b int) int {
+// 	return a + b
+// }
+
+// unsuccessfull tests
 package main
 
-// Add returns a + b
-func Add(a, b int) int {
-	return a + b
-}
+import "testing"
 
+func TestSample(t *testing.T) {
+	// Force a failure
+	got := 1
+	want := 2
+
+	if got != want {
+		t.Fatalf("expected %d but got %d ❌", want, got)
+	}
+}
