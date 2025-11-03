6abf822a738ed5186135e9647e21e874d8856588
PrashantR30 <prashant@adg.io>
2025-11-03 14:20:51 +0400

    add test-2

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
-// Successful test
-package main
-
-// Add returns a + b
-func Add(a, b int) int {
-	return a + b
-}
-
-// unsuccessfull tests
+// // Successful test
 // package main
 
-// import "testing"
-
-// func TestSample(t *testing.T) {
-// 	// Force a failure
-// 	got := 1
-// 	want := 2
-
-// 	if got != want {
-// 		t.Fatalf("expected %d but got %d ❌", want, got)
-// 	}
+// // Add returns a + b
+// func Add(a, b int) int {
+// 	return a + b
 // }
+
+// unsuccessfull tests
+package main
+
+import "testing"
+
+func TestSample(t *testing.T) {
+	// Force a failure
+	got := 1
+	want := 2
+
+	if got != want {
+		t.Fatalf("expected %d but got %d ❌", want, got)
+	}
+}
